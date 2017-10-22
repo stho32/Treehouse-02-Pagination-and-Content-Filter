673ac4a1958512249b3402f935326de03d76ac7f
Stefan Hoffmann <sthoffmann@onlinehome.de>
2017-10-22 17:25:37 +0200

    Added more comments and documentation to be more helpful.

--- Source/js/unobstrusivePagingAndSearching.js
+++ Source/js/unobstrusivePagingAndSearching.js
@@ -50,11 +50,17 @@ function UnobstrusivePagingAndSearching(selector,
         Show Page 
     */
     function showPage(pageNumber) {
+        /* Since we know what page size we want to have
+           we calculate starting and ending indexes so we
+           know what to how and what not to show.
+        */
         let startIndex = pageNumber * pageSize;
         let endIndex = pageNumber * pageSize + pageSize -1;
         
         console.log("showing " + (startIndex + 1).toString() + " to " + (endIndex + 1).toString());
 
+        // note: This function works with the matched items, not with every item.
+        //       This way we can stack paging and searching. 
         for (let i = 0; i < matchedItems.length; i++ ) {
             if ( i >= startIndex && i <= endIndex ) {
                 /* These elements should be visible */
@@ -69,12 +75,18 @@ function UnobstrusivePagingAndSearching(selector,
     }
 
     /**
-     * Search Callback 
+     * Search Callback for the search control.
+     * 
+     * The search control calls this callback on the occasion of a search being performed;
      */
     function search(searchText) {
+        /* We count how many items we find ... */
         let visibleItemsCount = 0;
+        /* and remember them  */
         matchedItems = [];
         
+        /* We remove the current paging control from the DOM 
+           if its there... */
         if ( pagingControl !== undefined ) {
             pagingControl.remove();
             pagingControl == undefined;
@@ -82,10 +94,21 @@ function UnobstrusivePagingAndSearching(selector,
 
         for (let i = 0; i < $listItems.length; i++ ) {
             let shouldBeVisible = false;
+            /* To perform the search I've created the parameter "areYouAMatchCallback".
+               This is a callback. As I may have already stated I wanted to develop 
+               in a way that may enable me to grab the code later for real development. 
+               So it make sence to split the matching out to be able to react on different
+               list item markup then.
+               As always, one should check if the parameter has been passed to us first.
+               If it is undefined the functionality would break. */
             if ( areYouAMatchCallback !== undefined ) {
+                /* We pass a text and the item as jquery-thingy. 
+                   The callback can then grab whatever elements it needs and tell me, 
+                   if it is a match or not. */
                 shouldBeVisible = areYouAMatchCallback(searchText, $($listItems[i]));
             }
 
+            /* When the item should be visible, we make it so _and_ we remember it. */
             if ( shouldBeVisible ) {
                 visibleItemsCount += 1;
                 matchedItems.push($listItems[i]);
@@ -93,17 +116,30 @@ function UnobstrusivePagingAndSearching(selector,
             }
             else
             {
+                /* Else, we hide it */
                 $($listItems[i]).hide();
             }
         }
         console.log(matchedItems.length + " students matched");
 
         if ( visibleItemsCount == 0 ) {
+            /* Show "no students found" */
             $nothingFound.show()
         }
         else
         {
+            /* or do not show "no students found" */
             $nothingFound.hide();
+            /* If there are more then 10 results we apply our paging magic here. 
+               We pass where our list is and the page count. The page count is calculated over
+               the number of the matched items. 
+               The callback "showPage" will also operate on the matchedItems. 
+               So note how this works: 
+               1) we hide all items that are not matched by the search and show all 
+                  items that are matched.
+               2) we use paging magic, but only on the set of matched items that are 
+                  visible after this search. The paging will then decide, which items
+                  are to be hidden in the next step so that the paging works. */
             if ( visibleItemsCount > 10) {
                 pagingControl = PagingControl($list.parent(), Math.ceil(matchedItems.length / pageSize), showPage);
                 pagingControl.setActivePage(0);
--- readme.md
+++ readme.md
@@ -12,3 +12,21 @@ In short:
  - You shall not use jquery plugins.
  - Be unobstrusive.
 
+
+You can find the entry points in the "Source" directory: 
+
+index.html
+student-list-examples/44students.html
+student-list-examples/64students.html
+
+Although I have included paging, filtering and paging and filtering as separate 
+files and made them easy to switch, in my final testing round I only tested the last
+one to get my chance on exceeding expectations.
+
+I have tested the functionality in :
+
+- chrome 61.0.3163.100
+- FF 56
+- safari 
+
+And had much fun doing the coding in vscode. 
