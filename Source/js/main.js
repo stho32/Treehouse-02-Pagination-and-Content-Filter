5a21e01fa1de4e643f1c02b3f0144f4d7bf16854
Stefan Hoffmann <sthoffmann@onlinehome.de>
2017-10-22 15:45:09 +0200

    Added a comment..

--- Source/js/main.js
+++ Source/js/main.js
@@ -11,7 +11,10 @@
 /* searching only */
 UnobstrusiveSearching(".page-header", ".student-list",
 function(searchText, $element) {
-    console.log($element.find("h3").text());
+    /* I coded most of the challenge as if I'd write controls that 
+       would work with other lists, too. 
+       So here is what I use as text-matching. 
+    */
     if ( $element.find("h3").text().indexOf(searchText) > -1 )
         return true;
     if ( $element.find(".email").text().indexOf(searchText) > -1 )
